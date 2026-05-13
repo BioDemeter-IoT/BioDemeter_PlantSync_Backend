1cb8259d62902b90ad05c5808d641497a46ab943
Kevin <171992754+KevCast1604@users.noreply.github.com>
2026-05-12 19:20:26 -0500

    test update Plant History query service with checklist

--- src/main/java/com/plantsync/platform/plantprofiles/application/internal/queryservices/PlantHistoryQueryServiceImpl.java
+++ src/main/java/com/plantsync/platform/plantprofiles/application/internal/queryservices/PlantHistoryQueryServiceImpl.java
@@ -14,8 +14,7 @@ import java.util.Optional;
 
 
 @Service
-public class PlantHistoryQueryServiceImpl implements PlantHistoryQueryService
-{
+public class PlantHistoryQueryServiceImpl implements PlantHistoryQueryService {
 
     private final PlantHistoryRepository plantHistoryRepository;
 
